75e96e16e69c11c14c8e44eb8523c8ba3d24d436
Łukasz Bartnicki <luki9112@gmail.com>
2019-08-31 18:53:21 +0200

    koniec

--- e_Tablice/Main5.java
+++ e_Tablice/Main5.java
@@ -2,7 +2,14 @@
 public class Main5 {
 
 	public static void main(String[] args) {
-
+		int numbers[] = { -11, -2, -3, -1, -4, -16, -7, -8, -9, -10 };
+		int max = numbers[0];
+		for (int i = 0; i < numbers.length; i++){
+			if(numbers[i] > max){
+				max = numbers[i];
+			}
+		}
+		System.out.println(max);
 	}
 
 }
--- e_Tablice/Main7.java
+++ e_Tablice/Main7.java
@@ -5,8 +5,21 @@ public class Main7 {
 
 	public static void main(String[] args) {
 		Random generator = new Random();
-		generator.nextInt(21);
-
+	//	generator.nextInt(21);
+		int randNumbers[]= new int[100];
+		for (int i = 0; i <100; i++){
+			randNumbers[i] = generator.nextInt(21);
+			if (i == randNumbers.length-1){
+				System.out.println(randNumbers[i]);
+			}else{
+				System.out.print(randNumbers[i] + ", ");
+			}
+		}
+		int sum =0;
+		for (int j = 0; j < randNumbers.length; j++){
+			sum += randNumbers[j];
+		}
+		System.out.println("Suma 100 losowych liczb od 0 do 20 to: " + sum);
 	}
 
 }
